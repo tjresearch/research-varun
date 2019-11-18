56e5633609d2eee789eb031b15d022612a435fe5
2020vbhatnag <57716241+2020vbhatnag@users.noreply.github.com>
2019-11-18 14:12:00 -0500

    Create TUI.py

---
+++ TUI.py
@@ -0,0 +1,24 @@
+import curses
+from curses import wrapper
+
+
+stdcsr = curses.initscr()
+curses.noecho()
+stdcsr.keypad(True)
+
+def exit():
+    curses.nocbreak()
+    stdcsr.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+def main(stdcsr):
+    stdcsr.clear()
+    pad = curses.newpad(100,100)
+    for y in range(0, 99):
+        for x in range(0, 99):
+            pad.addch(y, x, ord('a') + (x * x + y * y) % 26)
+
+    #pad.refresh(0, 0, 5, 5, 20, 75)
+
+wrapper(main)
