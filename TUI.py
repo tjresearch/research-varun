378b2a107d4ff42384a986969b90f63585890647
AlterixAsi <bhatnagarvarun12@gmail.com>
2019-11-18 13:45:57 -0500

    pls

--- TUI.py
+++ TUI.py
@@ -1,91 +1,26 @@
-#!/usr/bin/env python
-#
-# Urwid tour.  It slices, it dices..
-#    Copyright (C) 2004-2011  Ian Ward
-#
-#    This library is free software; you can redistribute it and/or
-#    modify it under the terms of the GNU Lesser General Public
-#    License as published by the Free Software Foundation; either
-#    version 2.1 of the License, or (at your option) any later version.
-#
-#    This library is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public
-#    License along with this library; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# Urwid web site: http://excess.org/urwid/
-
-"""
-Urwid tour.  Shows many of the standard widget types and features.
-"""
-
-import urwid
-import urwid.raw_display
-import urwid.web_display
-
-def main():
-    text_header = (u"Welcome to the urwid tour!  "
-        u"UP / DOWN / PAGE UP / PAGE DOWN scroll.  F8 is the EMRGENCY EXIT (DONT USE UNLESS BACKED UP) exits.")
-    text_button_list = [u"Backups", u"Exceptions", u"Active Threads", u"Close" , u"Help"]
+import curses
+from curses import wrapper
 
 
-    def button_press(button):
-        frame.footer = urwid.AttrWrap(urwid.Text(
-            [u"Pressed: ", button.get_label()]), 'header')
+stdcsr = curses.initscr()
+curses.noecho()
+stdcsr.keypad(True)
 
-    #blank = urwid.Divider()
-    listbox_content = [
+def exit():
+    curses.nocbreak()
+    stdcsr.keypad(False)
+    curses.echo()
+    curses.endwin()
 
-        #blank,
-        urwid.Padding(urwid.GridFlow(
-            [urwid.AttrWrap(urwid.Button(txt, button_press),
-                'buttn','buttnf') for txt in text_button_list],
-            15, 3, 2, 'left'),
-            left=3, right=3, min_width=13),
-        #blank,
-        ]
-    header = urwid.AttrWrap(urwid.Text(text_header), 'header')
-    listbox = urwid.ListBox(urwid.SimpleListWalker(listbox_content))
-    frame = urwid.Frame(urwid.AttrWrap(listbox, 'body'), header=header)
+def main(stdcsr):
+    stdcsr.clear()
+    pad = curses.newpad(100,100)
+    for y in range(0, 99):
+        for x in range(0, 99):
+            pad.addch(y, x, ord('a') + (x * x + y * y) % 26)
 
-    palette = [
-        ('body','black','light gray', 'standout'),
-        ('reverse','light gray','black'),
-        ('header','white','dark red', 'bold'),
-        ('important','dark blue','light gray',('standout','underline')),
-        ('editfc','white', 'dark blue', 'bold'),
-        ('editbx','light gray', 'dark blue'),
-        ('editcp','black','light gray', 'standout'),
-        ('bright','dark gray','light gray', ('bold','standout')),
-        ('buttn','black','dark cyan'),
-        ('buttnf','white','dark blue','bold'),
-        ]
+    #pad.refresh(0, 0, 5, 5, 20, 75)
+
+wrapper(main)
 
 
-    # use appropriate Screen class
-    if urwid.web_display.is_web_request():
-        screen = urwid.web_display.Screen()
-    else:
-        screen = urwid.raw_display.Screen()
-
-    def unhandled(key):
-        if key == 'f8':
-            raise urwid.ExitMainLoop()
-
-    urwid.MainLoop(frame, palette, screen,
-        unhandled_input=unhandled).run()
-
-def setup():
-    urwid.web_display.set_preferences("Urwid Tour")
-    # try to handle short web requests quickly
-    if urwid.web_display.handle_short_request():
-        return
-
-    main()
-
-if '__main__'==__name__ or urwid.web_display.is_web_request():
-    setup()
