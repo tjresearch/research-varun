cc963679c957286878384f54713d4ca0543cf4cb
AlterixAsi <bhatnagarvarun12@gmail.com>
2019-11-20 09:57:00 -0500

    TUI class done needs to be attached to DRIVER

--- DRIVER.py
+++ DRIVER.py
@@ -7,13 +7,16 @@ from selenium import webdriver
 import time
 import os
 import threading
+import curses
+import curses.textpad
+from sys import getsizeof
 
 
 reloadTimelines = False
 reloadInteractions = False
 reloadRetweets = False
 reloadReplies = False
-useSavedData = False
+useSavedData = True
 
 quitt = False
 
@@ -26,9 +29,9 @@ save = {}
 api = tweepy.API()
 stats = {}
 
-timelineNodeA = 2
+timelineNodeA = 1
 replyNodeA = 1
-analyzeNodeA = 2
+analyzeNodeA = 1
 
 timelineNodes = []
 replyNode = []
@@ -37,6 +40,9 @@ analyzeNode = []
 tNodeStatus = [0] * timelineNodeA
 rNodeStatus = [0] * replyNodeA
 aNodeStatus = [0] * analyzeNodeA
+
+exceptions = Queue()
+backupInfo = ["last whole update", "lastB backup update + size", "lastS statsbackup update + size", "backupnow", "back"]
 
 
 def initThreads():
@@ -69,11 +75,12 @@ idQueue.put(u1.id)
 def backupThread(x:int):
     while True:
         try:
+            now = datetime.now()
+            dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
             with open("save.json") as json_file:
-                now = datetime.now()
-                dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
                 with open("backups/backup_" + dt_string + ".json", "w+") as json_file:
                     json.dump(save, json_file, indent=4)
+            backupInfo[1] = "lastB " + dt_string + " " + str(getsizeof(save))
         except:
             now = datetime.now()
             print("Warning couldnt backup " + now.strftime("%d_%m_%Y_%H_%M_%S"))
@@ -336,4 +343,194 @@ for x in range(0, analyzeNodeA):
 backup = threading.Thread(target=backupThread,daemon=True,args=[0])
 backup.start()
 #endNode.start()
-endNodeJob()
+endNode = threading.Thread(target=endNodeJob, daemon=True, args=[0])
+
+sel = 0
+class Screen(object):
+    UP = -1
+    DOWN = 1
+
+    def __init__(self, items):
+        """ Initialize the screen window
+        Attributes
+            window: A full curses screen window
+            width: The width of `window`
+            height: The height of `window`
+            max_lines: Maximum visible line count for `result_window`
+            top: Available top line position for current page (used on scrolling)
+            bottom: Available bottom line position for whole pages (as length of items)
+            current: Current highlighted line number (as window cursor)
+            page: Total page count which being changed corresponding to result of a query (starts from 0)
+            ┌--------------------------------------┐
+            |1. Item                               |
+            |--------------------------------------| <- top = 1
+            |2. Item                               |
+            |3. Item                               |
+            |4./Item///////////////////////////////| <- current = 3
+            |5. Item                               |
+            |6. Item                               |
+            |7. Item                               |
+            |8. Item                               | <- max_lines = 7
+            |--------------------------------------|
+            |9. Item                               |
+            |10. Item                              | <- bottom = 10
+            |                                      |
+            |                                      | <- page = 1 (0 and 1)
+            └--------------------------------------┘
+        Returns
+            None
+        """
+        self.window = None
+
+        self.width = 0
+        self.height = 0
+
+        self.init_curses()
+
+        self.items = items
+
+        self.max_lines = curses.LINES
+        self.top = 0
+        self.bottom = len(self.items)
+        self.current = 0
+        self.page = self.bottom // self.max_lines
+
+    def init_curses(self):
+        """Setup the curses"""
+        self.window = curses.initscr()
+        self.window.keypad(True)
+
+        curses.noecho()
+        curses.cbreak()
+
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_CYAN)
+
+        self.current = curses.color_pair(2)
+
+        self.height, self.width = self.window.getmaxyx()
+
+    def run(self):
+        """Continue running the TUI until get interrupted"""
+        try:
+            input_stream(self)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            curses.endwin()
+
+
+
+
+    def scroll(self, direction)->bool:
+        """Scrolling the window when pressing up/down arrow keys"""
+        # next cursor position after scrolling
+        next_line = self.current + direction
+
+        # Up direction scroll overflow
+        # current cursor position is 0, but top position is greater than 0
+        if (direction == self.UP) and (self.top > 0 and self.current == 0):
+            self.top += direction
+            return True
+        # Down direction scroll overflow
+        # next cursor position touch the max lines, but absolute position of max lines could not touch the bottom
+        if (direction == self.DOWN) and (next_line == self.max_lines) and (self.top + self.max_lines < self.bottom):
+            self.top += direction
+            return True
+        # Scroll up
+        # current cursor position or top position is greater than 0
+        if (direction == self.UP) and (self.top > 0 or self.current > 0):
+            self.current = next_line
+            return True
+        # Scroll down
+        # next cursor position is above max lines, and absolute position of next cursor could not touch the bottom
+        if (direction == self.DOWN) and (next_line < self.max_lines) and (self.top + next_line < self.bottom):
+            self.current = next_line
+            return True
+        return False
+
+    def paging(self, direction):
+        """Paging the window when pressing left/right arrow keys"""
+        current_page = (self.top + self.current) // self.max_lines
+        next_page = current_page + direction
+        # The last page may have fewer items than max lines,
+        # so we should adjust the current cursor position as maximum item count on last page
+        if next_page == self.page:
+            self.current = min(self.current, self.bottom % self.max_lines - 1)
+
+        # Page up
+        # if current page is not a first page, page up is possible
+        # top position can not be negative, so if top position is going to be negative, we should set it as 0
+        if (direction == self.UP) and (current_page > 0):
+            self.top = max(0, self.top - self.max_lines)
+            return
+        # Page down
+        # if current page is not a last page, page down is possible
+        if (direction == self.DOWN) and (current_page < self.page):
+            self.top += self.max_lines
+            return
+
+    def display(self):
+        """Display the items on window"""
+        self.window.erase()
+        for idx, item in enumerate(self.items[self.top:self.top + self.max_lines]):
+            # Highlight the current cursor line
+            if idx == self.current:
+                self.window.addstr(idx, 0, item, curses.color_pair(2))
+            else:
+                self.window.addstr(idx, 0, item, curses.color_pair(1))
+        self.window.refresh()
+
+def input_stream(self):
+    global sel
+    """Waiting an input and run a proper method according to type of input"""
+    while True:
+        self.display()
+        ch = self.window.getch()
+        if ch == curses.KEY_UP:
+            if(self.scroll(self.UP)):
+                sel -= 1
+        elif ch == curses.KEY_DOWN:
+            if(self.scroll(self.DOWN)):
+                sel += 1
+        elif ch == curses.KEY_LEFT:
+            self.paging(self.UP)
+        elif ch == curses.KEY_RIGHT:
+            self.paging(self.DOWN)
+        elif ch == curses.KEY_ENTER:
+            break
+        elif ch == curses.ascii.ESC:
+            break
+
+
+def main():
+    switchboard("nah")
+
+def switchboard(type:str):
+    global sel
+    notExit = True
+    lines = ['status', "threads", "backup", "Close Program"]
+    while(notExit):
+        if(type == "backup"):
+            lines = ["last succesful update 15 min ago", "backup + time + time", "statsbackup + time + size", "exceptions + 7","allbackup","back"]
+        elif(type == "threads"):
+            lines = ["thread1 + job + status", "thread2 + job + status", "thread3 + job + status", "back"]
+        elif(type == "status"):
+            lines = ["56 succesfully analyzed", "preliminary on 107", "job thread waiting for api call", "back"]
+        elif(type == "back"):
+            lines = ['status', "threads", "backup", "Close Program"]
+        elif(type == "Close"):
+            notExit = False
+            continue
+        screen = Screen(lines)
+        screen.run()
+        type = lines[sel].split(" ")[0]
+        sel = 0
+        # try:
+        #     type = lines[sel].split(" ")[0]
+        # except IndexError:
+        #     type = "back"
+        #     print(sel)
+if __name__ == '__main__':
+    main()
